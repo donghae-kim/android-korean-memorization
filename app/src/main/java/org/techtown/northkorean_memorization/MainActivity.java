1a8d2c1e4403f97f53a8c4d3eb8f93115965710a
rbgus2002 <62997391+rbgus2002@users.noreply.github.com>
2021-11-30 03:45:51 +0900

    Implement DarkMode + Modify Button Color

--- app/src/main/java/org/techtown/northkorean_memorization/MainActivity.java
+++ app/src/main/java/org/techtown/northkorean_memorization/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
     private Button study;
@@ -17,6 +19,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //각 액티비티에 다크모드 구현 위해 존재해야하는 if문 문장
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.DarkTheme);
+        } else {
+            setTheme(R.style.LightTheme);
+        }
+
         setContentView(R.layout.activity_main);
 
         study = findViewById(R.id.study);
--- app/src/main/java/org/techtown/northkorean_memorization/Setting.java
+++ app/src/main/java/org/techtown/northkorean_memorization/Setting.java
@@ -1,5 +1,43 @@
 package org.techtown.northkorean_memorization;
 
-public class Setting {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.preference.PreferenceActivity;
+import android.view.View;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class Setting extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.DarkTheme);
+        } else {
+            setTheme(R.style.LightTheme);
+        }
+
+        setContentView(R.layout.activity_setting);
+//
+        TextView tvChangeTheme = findViewById(R.id.darkMode);
+        tvChangeTheme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }
+
+                finish();
+                startActivity(new Intent(Setting.this, Setting.this.getClass()));
+            }
+        });
+    }
 }
