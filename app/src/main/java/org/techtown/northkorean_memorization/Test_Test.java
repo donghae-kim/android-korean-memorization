d24b92555c70aaecd73c4f02ba0ce727311740ab
Arkady1356 <86937581+Arkady1356@users.noreply.github.com>
2021-12-06 22:42:33 +0900

    Test부분 깔짝 + 암기갯수 함수 업로드

--- app/src/main/java/org/techtown/northkorean_memorization/Test_Test.java
+++ app/src/main/java/org/techtown/northkorean_memorization/Test_Test.java
@@ -3,9 +3,13 @@ package org.techtown.northkorean_memorization;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,9 +26,12 @@ public class Test_Test extends AppCompatActivity implements View.OnClickListener
     private final String databaseName = "Words.db";
 
     TextView problems;
-    Button ans1, ans2, ans3, ans4;
+    //Button ans1, ans2, ans3, ans4;
+    Button ans[] = new Button[4];
     Button bookMark;
     Button passBtn;
+    ImageView correctSymbol;
+    ImageView unCorrectSymbol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +42,12 @@ public class Test_Test extends AppCompatActivity implements View.OnClickListener
         PreCreateDB.copyDB(this);
 
         // View 객체 획득
+        int[] a = {1, 2, 3, 4, 5};
+        updateDBdd(a, a);
+
         test_getView();
+        int temp = getMemorizedCount();
+        Log.d("Test_Test", temp + "Loaded ");
 
         problemSet = test_buildProblem(problemsNum, 1, false);
 
@@ -100,6 +112,7 @@ public class Test_Test extends AppCompatActivity implements View.OnClickListener
 
     private Problem test_makeProblem(Cursor cursor, int index, int loadedSize, int from, int to) {
         int id = cursor.getInt(0);
+        int field = cursor.getInt(4);
         String problem = cursor.getString(from);
         String correct = cursor.getString(to);
 
@@ -115,7 +128,8 @@ public class Test_Test extends AppCompatActivity implements View.OnClickListener
         cursor.moveToPosition((int) (Math.random() * loadedSize));
         String unCorrect3 = cursor.getString(to);
 
-        return (new Problem(id, problem, correct, unCorrect1, unCorrect2, unCorrect3));
+        int correctPos = (int)(Math.random() * 4);
+        return (new Problem(id, field, problem, correct, unCorrect1, unCorrect2, unCorrect3, correctPos));
     }
 
     private void test_rand_noDuple(int[] arr, int arrSize, int range) {
@@ -137,10 +151,13 @@ public class Test_Test extends AppCompatActivity implements View.OnClickListener
     void test_setActivity(int problemNumber) {
         Log.d("Test_Test", "ProblemSet " + problemNumber + " loading : ");
         problems.setText(problemSet[problemNumber].problem);
-        ans1.setText(problemSet[problemNumber].correct);
-        ans2.setText(problemSet[problemNumber].unCorrect1);
-        ans3.setText(problemSet[problemNumber].unCorrect2);
-        ans4.setText(problemSet[problemNumber].unCorrect3);
+
+        ans[problemSet[problemNumber].correctPos].setText(problemSet[problemNumber].correct);
+        int unCorIndex = 0;
+        for (int i = 0; i < 4; ++i) {
+            if(i != problemSet[problemNumber].correctPos)
+                ans[i].setText(problemSet[problemNumber].unCorrect[unCorIndex++]);
+        }
     }
 
     /**
@@ -150,13 +167,16 @@ public class Test_Test extends AppCompatActivity implements View.OnClickListener
     private void test_getView() {
         problems = (TextView) findViewById(R.id.test_problems);
 
-        ans1 = (Button) findViewById(R.id.test_ans1);
-        ans2 = (Button) findViewById(R.id.test_ans2);
-        ans3 = (Button) findViewById(R.id.test_ans3);
-        ans4 = (Button) findViewById(R.id.test_ans4);
+        ans[0] = (Button) findViewById(R.id.test_ans1);
+        ans[1] = (Button) findViewById(R.id.test_ans2);
+        ans[2] = (Button) findViewById(R.id.test_ans3);
+        ans[3] = (Button) findViewById(R.id.test_ans4);
 
         bookMark = (Button) findViewById(R.id.test_bookMark);
         passBtn = (Button) findViewById(R.id.test_passBtn);
+
+        correctSymbol = (ImageView) findViewById(R.id.test_correct_symbol);
+        unCorrectSymbol = (ImageView) findViewById(R.id.test_uncorrect_symbol);
     }
 
     /**
@@ -166,10 +186,10 @@ public class Test_Test extends AppCompatActivity implements View.OnClickListener
     private void test_setClick() {
         problems.setOnClickListener(this);
 
-        ans1.setOnClickListener(this);
-        ans2.setOnClickListener(this);
-        ans3.setOnClickListener(this);
-        ans4.setOnClickListener(this);
+        ans[0].setOnClickListener(this);
+        ans[1].setOnClickListener(this);
+        ans[2].setOnClickListener(this);
+        ans[3].setOnClickListener(this);
 
         bookMark.setOnClickListener(this);
         passBtn.setOnClickListener(this);
@@ -180,37 +200,98 @@ public class Test_Test extends AppCompatActivity implements View.OnClickListener
         Toast testToast = Toast.makeText(this.getApplicationContext(), "Touch!", Toast.LENGTH_SHORT);
         testToast.show();
 
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_up);
         if (chance == true) {
             chance = false;
+            if (view == ans[problemSet[solvedNum].correctPos]) {
+                Log.d("Test_Test", "Select Right Ans!");
+                correctSymbol.setVisibility(View.VISIBLE);
+                correctSymbol.startAnimation(animation);
+                collectNum++;
+            }
+            else {
+                Log.d("Test_Test", "Select X Ans!");
+                unCorrectSymbol.setVisibility(View.VISIBLE);
+                unCorrectSymbol.startAnimation(animation);
+            }
+
+            ++solvedNum;
         } else if (++solvedNum < problemsNum) {
             chance = true;
+            correctSymbol.setVisibility(View.INVISIBLE);
+            unCorrectSymbol.setVisibility(View.INVISIBLE);
             Log.d("Test_Test", "Next ProblemSet load : ");
             test_setActivity(solvedNum);
         } else {
             Log.d("Test_Test", "all ProblemSet is done");
         }
     }
+
+
+    private int getMemorizedCount() {
+        Test_DatabaseAdapter.DatabaseHelper helper = new Test_DatabaseAdapter.DatabaseHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        Cursor cursor = db.rawQuery("select * from Words where Memorized=1", null);
+        int loadedSize = cursor.getCount();
+
+        db.close();
+
+        return loadedSize;
+    }
+
+    public Boolean updateDBdd(int[] bookMark, int[] memorized) {
+
+        Test_DatabaseAdapter.DatabaseHelper helper = new Test_DatabaseAdapter.DatabaseHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        if (db != null) {
+
+            for (int i = 0; i < bookMark.length; ++i) {
+                updateSQLee(db, tableName, "BookMark", bookMark[i], 1);
+                Log.d("Test_Test_updateDB", bookMark[i] + "th BookMark change into 1");
+            }
+            for (int i = 0; i < memorized.length; ++i) {
+                updateSQLee(db, tableName, "Memorized", memorized[i], 1);
+                Log.d("Test_Test_updateDB", memorized[i] + "th Memorized change into 1");
+            }
+            db.close();
+        }
+        else
+            return false;
+
+        return true;
+    }
+
+    /**
+     * SQL문을 함수형태로 좀더 쉽게 만든건데 쉽게 느껴지려나..? 나중에도 쓸것같아서 만들어 둠
+     * db의 inputTableName에서 id번째 행의 what 속성을 Value로 바꿈
+     */
+    private void updateSQLee(SQLiteDatabase db, String inputTableName, String what, int id, int value) {
+        db.execSQL("UPDATE " + tableName + " SET " + what + " = 1 where _id = " + id);
+    }
 }
 
 class Problem {
     int id;
-    int filed;
+    int field;
     String problem;
     String correct;
-    String unCorrect1;
-    String unCorrect2;
-    String unCorrect3;
+    String[] unCorrect = new String[3];
+    int correctPos;
 
-    public Problem(int id, String problem, String correct, String unCorrect1, String unCorrect2, String unCorrect3) {
+    public Problem(int id, int field, String problem, String correct, String unCorrect0, String unCorrect1, String unCorrect2, int correctPos) {
         this.id = id;
+        this.field = field;
         this.problem = problem;
         this.correct = correct;
-        this.unCorrect1 = unCorrect1;
-        this.unCorrect2 = unCorrect2;
-        this.unCorrect3 = unCorrect3;
+        this.unCorrect[0] = unCorrect0;
+        this.unCorrect[1] = unCorrect1;
+        this.unCorrect[2] = unCorrect2;
+        this.correctPos = correctPos;
     }
 
     public void printAll() {
-        Log.d("Test_Test", id + " " + problem + " " + correct + " " + unCorrect1 + " " + unCorrect2 + " " + unCorrect3);
+        Log.d("Test_Test", id + " " + field + " "+ problem + " " + correct + " " + unCorrect[0] + " " + unCorrect[1] + " " + unCorrect[2] + " " + correctPos);
     }
 }
