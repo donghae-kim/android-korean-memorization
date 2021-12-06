5b111ffbf9e6e3779421d0aaffc7f9f0a19ef5c7
Arkady1356 <86937581+Arkady1356@users.noreply.github.com>
2021-12-06 17:27:06 +0900

    temp update of CodeConductor

--- app/src/main/java/org/techtown/northkorean_memorization/CodeConductor.java
+++ app/src/main/java/org/techtown/northkorean_memorization/CodeConductor.java
@@ -9,19 +9,19 @@ public class CodeConductor extends AppCompatActivity {
     private final String tableName = "Words";
     private final String databaseName = "Words.db";
 
-    public Boolean updateDB(int[] bookMark, int[] memorized) {
+    public Boolean updateDBdd(int[] bookMark, int[] memorized) {
 
         Test_DatabaseAdapter.DatabaseHelper helper = new Test_DatabaseAdapter.DatabaseHelper(this);
         SQLiteDatabase db = helper.getWritableDatabase();
 
         if (db != null) {
 
-            for (int i = 0; i < bookMark.length+1; ++i) {
-                updateSQL(db, tableName, "BookMark", bookMark[i], 1);
+            for (int i = 0; i < 1; ++i) {
+                updateSQLee(db, tableName, "BookMark", bookMark[i], 1);
                 Log.d("Test_Test_updateDB", bookMark[i] + "th BookMark change into 1");
             }
-            for (int i = 0; i < memorized.length; ++i) {
-                updateSQL(db, tableName, "Memorized", memorized[i], 1);
+            for (int i = 0; i < 1; ++i) {
+                updateSQLee(db, tableName, "Memorized", memorized[i], 1);
                 Log.d("Test_Test_updateDB", memorized[i] + "th Memorized change into 1");
             }
             db.close();
@@ -36,7 +36,7 @@ public class CodeConductor extends AppCompatActivity {
      * SQL문을 함수형태로 좀더 쉽게 만든건데 쉽게 느껴지려나..? 나중에도 쓸것같아서 만들어 둠
      * db의 inputTableName에서 id번째 행의 what 속성을 Value로 바꿈
      */
-    private void updateSQL(SQLiteDatabase db, String inputTableName, String what, int id, int value) {
+    private void updateSQLee(SQLiteDatabase db, String inputTableName, String what, int id, int value) {
         db.execSQL("UPDATE " + tableName + " SET " + what + " = 1 where _id = " + id);
     }
 
--- app/src/main/java/org/techtown/northkorean_memorization/Study.java
+++ app/src/main/java/org/techtown/northkorean_memorization/Study.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.icu.text.IDNA;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,6 +23,9 @@ import androidx.appcompat.app.AppCompatDelegate;
 
 
 public class Study extends AppCompatActivity {
+
+    private final String tableName = "Words";
+
     Test_DatabaseAdapter.DatabaseHelper helper;
     Test_DatabaseAdapter databaseAdapter;
     String[] item = {"1. 일상생활어","2. IT용어","3. 은어"};
@@ -76,8 +80,8 @@ public class Study extends AppCompatActivity {
                             memo[i] = Integer.valueOf(checkitem[position]); //메모에다가 클릭시 받은 index값 int 값으로 넣기
                             System.out.println(memo[i]); //logcat
 
-                            CodeConductor cod = new CodeConductor();
-                           // cod.updateDB(memo,memo); // test 용으로 update db 에 memo 두개넣은거
+                            updateDB(memo,memo); // test 용으로 update db 에 memo 두개넣은거
+                            CodeConductor cd = new CodeConductor();
 //********************************************************************************************************** codeconductor 로 넘기는부분
                         }
 
@@ -176,6 +180,40 @@ public class Study extends AppCompatActivity {
 
 
     }
+
+
+    public Boolean updateDB(int[] bookMark, int[] memorized) {
+
+        Test_DatabaseAdapter.DatabaseHelper helper = new Test_DatabaseAdapter.DatabaseHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        if (db != null) {
+
+            for (int i = 0; i < 1; ++i) {
+                updateSQL(db, tableName, "BookMark", bookMark[i], 1);
+                Log.d("Test_Test_updateDB", bookMark[i] + "th BookMark change into 1");
+            }
+            for (int i = 0; i < 1; ++i) {
+                updateSQL(db, tableName, "Memorized", memorized[i], 1);
+                Log.d("Test_Test_updateDB", memorized[i] + "th Memorized change into 1");
+            }
+            db.close();
+        }
+        else
+            return false;
+
+        return true;
+    }
+
+    /**
+     * SQL문을 함수형태로 좀더 쉽게 만든건데 쉽게 느껴지려나..? 나중에도 쓸것같아서 만들어 둠
+     * db의 inputTableName에서 id번째 행의 what 속성을 Value로 바꿈
+     */
+    private void updateSQL(SQLiteDatabase db, String inputTableName, String what, int id, int value) {
+        db.execSQL("UPDATE " + tableName + " SET " + what + " = 1 where _id = " + id);
+    }
+
+
 
 
 
